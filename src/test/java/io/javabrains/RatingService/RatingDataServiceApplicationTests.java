793ad8e86d67c4b288557945df109ff1e7167349
Roy Eliyahu <RoyE@gmul-barak.co.il>
2019-07-01 12:40:50 +0300

    Initial commit

---
+++ src/test/java/io/javabrains/RatingService/RatingDataServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.javabrains.RatingService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RatingDataServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
