6b03f6ce5c2400c7619a0b11b51206fa4e1da4e9
RoyE <royeliyahu827@gmail.com>
2019-07-02 17:15:09 +0300

    running

---
+++ src/main/java/io/javabrains/RatingService/RatingResource.java
@@ -0,0 +1,14 @@
+package io.javabrains.RatingService;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingdata")
+public class RatingResource {
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId, 4);
+    }
+}
